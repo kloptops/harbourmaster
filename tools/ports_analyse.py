ed97f59a9959f0aadd299ed68bf07b28595b7eb4
Jacob <kloptops@gmail.com>
2023-04-15 19:12:35 +0800

    Updated tools/ports_analuse.py and Added ports_info.py

--- tools/ports_analyse.py
+++ tools/ports_analyse.py
@@ -6,19 +6,26 @@ import fnmatch
 import gzip
 import hashlib
 import json
-import pathlib
-import re
 import os
+import pathlib
+import pprint
+import re
 import shutil
+import subprocess
 import sys
 import tempfile
 import textwrap
 import zipfile
-import subprocess
+
 
 from difflib import Differ
 from pathlib import Path
 
+"""
+The scripts purpose is to analyse portmaster and build up enough information to make <portname>.port.json for any older ports.
+
+If a port has a port.json in it, it will load it.
+"""
 
 def hash_file(file):
     md5 = hashlib.md5()
@@ -43,42 +50,156 @@ def hash_text(text):
     return md5.hexdigest()
 
 
-def analyse_port(file_name, zip_hash, all_data):
+################################################################################
+## Port Information
+class PortInfo():
+    __attrs__ = (
+        'file', 'source', 'items', 'items_opt')
+
+    def __init__(self, info):
+        if isinstance(info, pathlib.PurePath):
+            with info.open('r') as fh:
+                self.from_dict(json.load(fh))
+
+        elif isinstance(info, dict):
+            self.from_dict(info)
+
+        else:
+            raise ValueError(str(info))
+
+    def from_dict(self, info):
+        self.file = info.get('file', None)
+        self.source = info.get('source', None)
+        self.items = info.get('items', None)
+        self.items_opt = info.get('items_opt', None)
+
+    def merge_info(self, other):
+        if isinstance(other, (str, dict)):
+            info = PortInfo(other)
+
+        elif isinstance(other, PortInfo):
+            pass
+
+        else:
+            raise ValueError(str(info))
+
+        for attr in self.__attrs__:
+            value_a = getattr(self, attr)
+            value_b = getattr(other, attr)
+
+            if value_a is None and value_b is not None:
+                setattr(self, attr, value_b)
+
+    def to_dict(self):
+        return {
+            attr: getattr(self, attr)
+            for attr in self.__attrs__
+            if getattr(self, attr) is not None
+            }
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+def clean_name(file_name, attr='name'):
+    if attr == 'name':
+        name = file_name.name
+    elif attr == 'stem':
+        name = file_name.stem
+    else:
+        name = str(file_name)
+
+    return name.lower().replace(" ", ".").replace("..", ".")
+
+def add_nicely(base_dict, key, value):
+    if key not in base_dict:
+        base_dict[key] = value
+        return
+
+    if isinstance(base_dict[key], str):
+        if base_dict[key] == value:
+            return
+
+        base_dict[key] = [base_dict[key]]
+
+    if value not in base_dict[key]:
+        base_dict[key].append(value)
+
+def analyse_port(file_name, all_data):
     # When we analyse a port, we want to look at the root directories, and the rooth scripts.
-    root_scripts = []
-    root_directories = []
-    print(file_name)
+    port_info_file = None
+    items = []
+    dirs = []
+    scripts = []
+
+    zip_name = clean_name(file_name)
+
     with zipfile.ZipFile(file_name, 'r') as zf:
         for file_info in zf.infolist():
             if file_info.filename.startswith('/'):
-                continue
+                ## Sneaky
+                print(f"Port {zip_name} has an illegal file {file_info.filename!r}.")
+
+            if file_info.filename.startswith('../'):
+                ## Little
+                print(f"Port {zip_name} has an illegal file {file_info.filename!r}.")
+
+            if '/../' in file_info.filename:
+                ## Shits
+                print(f"Port {zip_name} has an illegal file {file_info.filename!r}.")
 
             if '/' in file_info.filename:
-                root_directory = file_info.filename.split('/', 1)[0]
-                if root_directory not in root_directories:
-                    root_directories.append(root_directory)
+                parts = file_info.filename.split('/')
+
+                if parts[0] not in dirs:
+                    items.append(parts[0] + '/')
+                    dirs.append(parts[0])
+
+                if len(parts) == 2:
+                    if parts[1].lower().endswith('.port.json'):
+                        ## TODO: add the ability for multiple port folders to have multiple port.json files. ?
+                        if port_info_file is not None:
+                            print(f"Port {zip_name} has multiple port.json files.")
+                            print(f"- Before: {port_info_file!r}")
+                            print(f"- Now:    {file_info.filename!r}")
+
+                        port_info_file = file_info.filename
+
             else:
-                file_hash = hash_text(zf.read(file_info.filename))
+                if file_info.filename.lower().endswith('.sh'):
+                    scripts.append(file_info.filename)
+                    items.append(file_info.filename)
+                else:
+                    print(f"Port {zip_name} contains {file_info.filename} at the top level, but it is not a shell script.")
 
-                root_scripts.append([file_info.filename, file_info.file_size, file_hash])
+        if port_info_file is not None:
+            with zf.open(port_info_file, "r") as fh:
+                port_info = PortInfo(json.loads(fh.read()))
+        else:
+            port_info = PortInfo({})
 
-    return {
-        'scripts': root_scripts,
-        'dirs': root_directories,
-        }
+    ## These two are always overriden.
+    port_info.items = items
+    port_info.source = f"pm/{zip_name}"
 
-def append_to_list(the_list, item):
-    the_list.append(item)
-
-def prepend_to_list(the_list, item):
-    the_list.insert(0, item)
-
-def analyse_ports(root_path, commit_id, all_data, is_old=True):
-    if is_old:
-        add_mode = append_to_list
+    if port_info_file is None:
+        port_info.file = f"{dirs[0]}/{(clean_name(file_name, 'stem') + '.port.json')}"
     else:
-        add_mode = prepend_to_list
+        port_info.file = port_info_file
 
+    if port_info.items_opt is not None:
+        for item in port_info.items_opt:
+            add_nicely(all_data['items'], item, zip_name)
+
+    for item in items:
+        add_nicely(all_data['items'], item, zip_name)
+
+    all_data['ports'][zip_name] = port_info.to_dict()
+
+
+def analyse_ports(root_path, all_data):
     zip_files = {}
     print(f"Checking {root_path}")
     for sub_file in root_path.iterdir():
@@ -89,46 +210,15 @@ def analyse_ports(root_path, commit_id, all_data, is_old=True):
         if sub_file.suffix not in ('.zip', ):
             continue
 
-        if sub_file.name.lower() == 'portmaster.zip':
+        zip_name = clean_name(sub_file)
+
+        if zip_name in ('portmaster.zip', 'fallout 1.zip'):
             continue
 
-        file_hash = hash_file(sub_file)
+        if zip_name in all_data['ports']:
+            continue
 
-        file_size = sub_file.stat().st_size
-
-        if file_hash in all_data['cache']:
-            zip_files[sub_file.name] = all_data['cache'][file_hash]
-        else:
-            zip_files[sub_file.name] = temp = {
-                'hash': file_hash,
-                'info': analyse_port(sub_file, file_hash, all_data),
-                }
-
-            all_data['cache'][file_hash] = temp
-
-        for script_file_name, script_file_size, script_file_hash in zip_files[sub_file.name]['info']['scripts']:
-            rsl = all_data['root_script_lookup'].setdefault(script_file_name, {}).setdefault(script_file_hash, [])
-            if file_hash not in rsl:
-                add_mode(rsl, file_hash)
-
-        for root_directory in zip_files[sub_file.name]['info']['dirs']:
-            rdl = all_data['root_directory_lookup'].setdefault(root_directory, [])
-            if sub_file.name not in rdl:
-                add_mode(rdl, sub_file.name)
-
-        zhl = all_data['zip_hash'].setdefault(sub_file.name, [])
-        if file_hash not in zhl:
-            add_mode(zhl, file_hash)
-
-        hzl = all_data['hash_zip'].setdefault(file_hash, [])
-        if sub_file.name not in hzl:
-            add_mode(hzl, sub_file.name)
-
-        zsl = all_data['zip_size'].setdefault(sub_file.name, {}).setdefault(str(file_size), [])
-        if file_hash not in zsl:
-            add_mode(zsl, file_hash)
-
-        # print(f"{commit_id[:10]}: {sub_file.name}, {file_hash}")
+        analyse_port(sub_file, all_data)
 
     return zip_files
 
@@ -147,59 +237,41 @@ def git_rewind(root_path, all_data):
         commit_ids.append(commit_id)
 
     try:
-        for i, commit_id in enumerate(commit_ids):
+        for i, commit_id in enumerate(commit_ids, 1):
             subprocess.check_output(['git', 'checkout', commit_id])
 
-            commits.append(analyse_ports(root_path, commit_id, all_data, is_old))
+            analyse_ports(root_path, all_data)
             print(f"Done: {i:3d} / {len(commit_ids):3d}")
+
+            if i > 1:
+                break
 
     except KeyboardInterrupt:
         pass
 
     subprocess.check_output(['git', 'checkout', 'main'])
 
-    return commits
-
-
 def main():
     root_path = Path('../Portmaster/').absolute()
-    info_file = Path('ports_info.json').absolute()
-    info_file_gz = Path('ports_info.json.gz').absolute()
-
-    cache_file_gz = Path('ports_info_cache.json.gz').absolute()
-
-    if cache_file_gz.is_file():
-        with gzip.open(cache_file_gz, 'rt') as gz:
-            cache = json.load(gz)
-    else:
-        cache = {
-            'first_run': True,
-            'first_checkout': None,
-            'last_checkout': None,
-            }
+    host_path = Path('../Portmaster-Hosting/').absolute()
+    info_file = Path('pylibs/ports_info.py').absolute()
 
     all_data = {
-        'cache': cache,
-        'hash_zip': {},
-        'root_directory_lookup': {},
-        'root_script_lookup': {},
-        'zip_hash': {},
-        'zip_size': {},
+        'items': {},
+        'ports': {},
         }
+
+    analyse_ports(host_path, all_data)
 
     os.chdir(root_path)
 
-    commits = git_rewind(root_path, all_data)
+    git_rewind(root_path, all_data)
 
-    del all_data['cache']
-    with info_file.open('w') as fh:
-        json.dump(all_data, fh, indent=4)
-
-    with gzip.open(info_file_gz, 'wt', 9) as gz:
-        json.dump(all_data, gz)
-
-    with gzip.open(cache_file_gz, 'wt', 1) as gz:
-        json.dump(cache, gz)
+    with info_file.open('wt') as fh:
+        fh.write( "# -- Autogenerated by tools/ports_analyse.py --\n")
+        fh.write(f"# -- {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} --\n\n")
+        out_str = json.dumps(all_data, indent=4, sort_keys=True)
+        fh.write(f"ports_info = {out_str}\n")
 
 
 if __name__ == '__main__':
