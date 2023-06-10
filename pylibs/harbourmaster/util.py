82e8557233b9f88228547260e43187bf8c7f2e70
Jacob <kloptops@gmail.com>
2023-06-10 16:24:02 +0800

    harbourmaster: moved from using functools.cache to functools.lru_cache as the former requires python 3.9

--- pylibs/harbourmaster/util.py
+++ pylibs/harbourmaster/util.py
@@ -89,7 +89,7 @@ def nice_size(size):
     return f"{size:.02f} {suffix}"
 
 
-@functools.cache
+@functools.lru_cache(maxsize=512)
 def name_cleaner(text):
     temp = re.sub(r'[^a-zA-Z0-9, _\-\.]+', '', text.strip().casefold())
     return re.sub(r'[ \.\-]+', '.', temp)
